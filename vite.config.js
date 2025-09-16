161ca64a5e77d719dbb3d3ab744923ec88581889
Rajdeep Samanta <samantarajdeep59@gmail.com>
2025-09-16 17:13:20 +0530

    d-11.5

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-// vite.config.js or vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/ //temporary
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
 export default defineConfig({
-  host:true,
+  base: '/',
+  host: true,
   plugins: [react(), tailwindcss()],
-  
 })
